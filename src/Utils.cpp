0cc0922c269d57a29df8cbd80e42f9b474020e17
Alessandro Giraudo <03alegiraudo@gmail.com>
2025-05-15 11:22:05 +0200

    Cura della grafica

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -15,141 +15,125 @@ namespace PolyhedralLibrary
 		{
 			//Cell0Ds
 			mesh.NumCell0Ds = 4;
-			mesh.Cell0DsId.resize(mesh.NumCell0Ds);
-
+			
+			mesh.Cell0DsId.resize(mesh.NumCell0Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
-			{
 				mesh.Cell0DsId[i] = i;
-			}
+			
+			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3); 	//Salvataggio delle coordinate
 
-			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3);
 			mesh.Cell0DsCoordinates <<
-				1,1,1,
-				-1,-1,1,
-				-1,1,-1,
+				1, 1, 1,
+			   -1,-1, 1,
+			   -1, 1,-1,
 				1,-1,-1;	
 				
 			mesh.Cell0DsCoordinates /= sqrt(3);
 
-			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)  //Salvataggio dei marker
 				mesh.Cell0DsMarker[1].push_back(mesh.Cell0DsId[i]);
 
 			//for(int i = 0; i < mesh.NumCell0Ds; i++)
 				//cout << mesh.Cell0DsMarker[1][i] << endl;
 
-
 			//cell1Ds
-
 			mesh.NumCell1Ds = 6;
-			mesh.Cell1DsId.resize(mesh.NumCell1Ds);
-
+			
+			mesh.Cell1DsId.resize(mesh.NumCell1Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
-			{
 				mesh.Cell1DsId[i] = i;
-			}
-			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds, 2);
-			mesh.Cell1DsExtrema << 
-			0, 1,
-			0, 2,
-			0, 3,
-			1, 2, 
-			1, 3,
-			2, 3;
 
-			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
+			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds, 2); //Salvataggio degli estremi dei lati
+			mesh.Cell1DsExtrema << 
+				0, 1,
+				0, 2,
+				0, 3,
+				1, 2, 
+				1, 3,
+				2, 3;
+
+			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)  //Salvataggio dei marker 
 				mesh.Cell1DsMarker[1].push_back(mesh.Cell1DsId[i]);
 
 			//Cell2Ds
 			mesh.NumCell2Ds = 4;
 
-			mesh.Cell2DsId.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsId.resize(mesh.NumCell2Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++)
-			{
 				mesh.Cell2DsId[i] = i;
-			}
 
-			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds); //Salvataggio dei vertici di ogni faccia
 			mesh.Cell2DsVertices[0] = {0, 1, 2};
 			mesh.Cell2DsVertices[1] = {0, 3, 1};
 			mesh.Cell2DsVertices[2] = {0, 2, 3};
 			mesh.Cell2DsVertices[3] = {1, 3, 2};
 
-			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds); //Salvataggio dei lati di ogni faccia
 			mesh.Cell2DsEdges[0] = {0, 3, 1};
 			mesh.Cell2DsEdges[1] = {2, 4, 0};
 			mesh.Cell2DsEdges[2] = {1, 5, 2};
 			mesh.Cell2DsEdges[3] = {4, 5, 3};
 					
-			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++) //Salvataggio dei marker
 				mesh.Cell2DsMarker[1].push_back(mesh.Cell2DsId[i]);
-			
-
 		}
 
-		
-		
 		// nel caso di ottaedro o cubo li tratto nello stesso modo, cioè come un ottaedro
 		// ottaedro
 		if((p==3 && q==4) || (p==4 && q==3))
 		{
+			//Cell0Ds
 			mesh.NumCell0Ds = 6;
-			mesh.Cell0DsId.resize(mesh.NumCell0Ds);
 
+			mesh.Cell0DsId.resize(mesh.NumCell0Ds);	//Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
-			{
 				mesh.Cell0DsId[i] = i;
-			}
 
-			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3);
+			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3); //Salvataggio delle coordinate
 			mesh.Cell0DsCoordinates <<
-				0,0,1,
-				1,0,0,
-				0,1,0,
-				-1,0,0,
-				0,-1,0,
-				0,0,-1;	
+				0, 0, 1,
+				1, 0, 0,
+				0, 1, 0,
+			   -1, 0, 0,
+				0,-1, 0,
+				0, 0,-1;	
 
-			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++) //Salvataggio dei marker
 				mesh.Cell0DsMarker[1].push_back(mesh.Cell0DsId[i]);
 			
-			// ce
-			
+			//Cell1Ds
 			mesh.NumCell1Ds = 12;
-			mesh.Cell1DsId.resize(mesh.NumCell1Ds);
 
+			mesh.Cell1DsId.resize(mesh.NumCell1Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
-			{
 				mesh.Cell1DsId[i] = i;
-			}
 
-			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds,2);
+			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds,2); //Salvataggio degli estremi dei lati
 			mesh.Cell1DsExtrema << 
-			0, 1,
-			0, 2,
-			0, 3,
-			0, 4,
-			5, 1,
-			5, 2,
-			5, 3,
-			5, 4,
-			1, 2,
-			2, 3,
-			3, 4,
-			4, 1;
+				0, 1,
+				0, 2,
+				0, 3,
+				0, 4,
+				5, 1,
+				5, 2,
+				5, 3,
+				5, 4,
+				1, 2,
+				2, 3,
+				3, 4,
+				4, 1;
 
-			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
-				mesh.Cell1DsMarker[1].push_back(mesh.Cell1DsId[i]);
+			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++) //Salvataggio dei marker
+				mesh.Cell1DsMarker[1].push_back(mesh.Cell1DsId[i]); 
 
 			//Cell2Ds
 			mesh.NumCell2Ds = 8;
 
-			mesh.Cell2DsId.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsId.resize(mesh.NumCell2Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++)
-			{
 				mesh.Cell2DsId[i] = i;
-			}
 				
-			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds); //Salvataggio dei vertici di ogni faccia
 			mesh.Cell2DsVertices[0] = {0,1,2};  
 			mesh.Cell2DsVertices[1] = {0,2,3};  
 			mesh.Cell2DsVertices[2] = {0,3,4};  
@@ -159,7 +143,7 @@ namespace PolyhedralLibrary
 			mesh.Cell2DsVertices[6] = {5,4,3};  
 			mesh.Cell2DsVertices[7] = {5,1,4};  
 
-			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds); //Salvataggio dei lati di ogni faccia
 			mesh.Cell2DsEdges[0] = {0, 8, 1};
 			mesh.Cell2DsEdges[1] = {1, 9, 2};
 			mesh.Cell2DsEdges[2] = {2,10, 3};
@@ -177,89 +161,83 @@ namespace PolyhedralLibrary
 		//icosaedro
 		if((p==3 && q==5) || (p==5 && q==3))
 		{
+			//Cell0Ds
 			mesh.NumCell0Ds = 12;
-			mesh.Cell0DsId.resize(mesh.NumCell0Ds);
 
+			mesh.Cell0DsId.resize(mesh.NumCell0Ds);	//Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
-			{
 				mesh.Cell0DsId[i] = i;
-			}
 
-			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3);
+			mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(mesh.NumCell0Ds, 3); //Salvataggio delle coordinate
 			mesh.Cell0DsCoordinates <<
-				0,1,phi,
-				0,-1,phi,
-				-phi,0,1,
-				-1,phi,0,
-				1,phi,0,
-				phi,0,1,
-				1,-phi,0,
-				phi,0,-1,
-				0,1,-phi,
-				-phi,0,-1,
-				0,-1,-phi,
-				-1,-phi,0;	
+				0,   1,  phi,
+				0,  -1,  phi,
+			 -phi,   0,    1,
+			   -1, phi,    0,
+				1, phi,    0,
+			  phi,   0,    1,
+				1,-phi,    0,
+			  phi,   0,   -1,
+				0,   1, -phi,
+			 -phi,   0,   -1,
+				0,  -1, -phi,
+			   -1, -phi,   0;	
 				
 			mesh.Cell0DsCoordinates /= sqrt(1+phi*phi);
 
-			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell0Ds; i++) //Salvataggio dei marker
 				mesh.Cell0DsMarker[1].push_back(mesh.Cell0DsId[i]);
 
 			// cell1Ds
-
 			mesh.NumCell1Ds = 30;
-			mesh.Cell1DsId.resize(mesh.NumCell1Ds);
 
+			mesh.Cell1DsId.resize(mesh.NumCell1Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
-			{
 				mesh.Cell1DsId[i] = i;
-			}
 
-			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds,2);
+			mesh.Cell1DsExtrema = Eigen::MatrixXi::Zero(mesh.NumCell1Ds,2); //Salvataggio degli estremi dei lati
 			mesh.Cell1DsExtrema << 
-			0,  1,
-			0,  2,
-			0,  4,
-			0,  5,
-			1,  2,
-			1,  6,
-			1,  5,
-			2,  3,
-			2,  9,
-			3,  4,
-			3,  9,
-			3,  8,
-			4,  8,
-			4,  5,
-			5,  7,
-			6,  7,
-			6, 10,
-			6,  1,
-			7, 10,
-			7,  5,
-			8,  9,
-			8, 11,
-			9, 11,
+			0,   1,
+			0,   2,
+			0,   4,
+			0,   5,
+			1,   2,
+			1,   6,
+			1,   5,
+			2,   3,
+			2,   9,
+			3,   4,
+			3,   9,
+			3,   8,
+			4,   8,
+			4,   5,
+			5,   7,
+			6,   7,
+			6,  10,
+			6,   1,
+			7,  10,
+			7,   5,
+			8,   9,
+			8,  11,
+			9,  11,
 			10, 11,
 			10,  6,
 			11,  2,
 			11,  3,
 			10,  7,
-			8,  4;
+			8,   4;
 
-			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell1Ds; i++) //Salvataggio dei marker
 				mesh.Cell1DsMarker[1].push_back(mesh.Cell1DsId[i]);
 
 			//Cell2Ds
 			mesh.NumCell2Ds = 20;
 
-			mesh.Cell2DsId.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsId.resize(mesh.NumCell2Ds); //Salvataggio degli ID
 			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++)
-			{
 				mesh.Cell2DsId[i] = i;
-			}
 			
-			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsVertices.resize(mesh.NumCell2Ds); //Salvataggio dei vertici di ogni faccia
 			mesh.Cell2DsVertices[0] = {0, 1, 2};
 			mesh.Cell2DsVertices[1] = {0, 2, 3};
 			mesh.Cell2DsVertices[2] = {0, 3, 4};
@@ -281,7 +259,7 @@ namespace PolyhedralLibrary
 			mesh.Cell2DsVertices[18] = {11,10,8};
 			mesh.Cell2DsVertices[19] = {10, 7,8};
 
-			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds);
+			mesh.Cell2DsEdges.resize(mesh.NumCell2Ds); //Salvataggio dei lati di ogni faccia
 			mesh.Cell2DsEdges[0] = {0, 4, 1};
 			mesh.Cell2DsEdges[1] = {1, 7,11};
 			mesh.Cell2DsEdges[2] = {11, 9, 2};
@@ -303,11 +281,9 @@ namespace PolyhedralLibrary
 			mesh.Cell2DsEdges[18] = {23,27,21};
 			mesh.Cell2DsEdges[19] = {27,18,19};
 
-			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++)
+			for(unsigned int i = 0; i < mesh.NumCell2Ds; i++) //Salvataggio dei marker
 				mesh.Cell2DsMarker[1].push_back(mesh.Cell2DsId[i]);
 		}
-		
-		
 	}
 	/*
 	bool BuildCell0Ds(PoliedralMesh& mesh, int p, int q, int b, int c)
@@ -317,9 +293,4 @@ namespace PolyhedralLibrary
 		mesh.Cell0DsCoordinates = Eigen::MatrixXd::Zero(3, mesh.NumCell0Ds);
 		
 	}*/
-	
-	
-	
-	
-	
 }
